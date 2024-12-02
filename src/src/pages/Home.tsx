45ce2c308fa9ac9b439357fb1508433a767a44ad
Thoriq Dharmawan <skyshi-thoriq.darmawan@edot.id>
2024-12-02 23:30:07 +0700

    feat: add a new card component

--- src/src/pages/Home.tsx
+++ src/src/pages/Home.tsx
@@ -1,9 +1,19 @@
+import Card from "@/components/custom/Card";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-xl font-semibold underline">
         Welcome to the Home Page
       </h1>
+
+      <div className="flex flex-row flex-wrap gap-x-3 gap-y-8">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
